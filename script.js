6a590fbe4c72817e6248dde9f4a8efe97748316c
ashubajara <banjaraashutosh@gmail.com>
2021-06-02 22:18:40 +0545

    Added functionality to main buttons, basic game logic done.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Game Variables
 let currentNumber = 0;
+let nums = [];
+let payout = 0;
+let credits = 0;
 let isAlive = false;
 
 // DOM Variables
@@ -13,12 +16,49 @@ let num1 = 10;
 let num2 = 12;
 
 function startGame(){
-  isAlive = true;
-  let num = getRandomNum();
-  currentNumber += num;
-  renderGame();
+  if (!isAlive){
+    isAlive = true;
+    let num = getRandomNum(1, 8);
+    nums.push(num);
+    currentNumber += num;
+    renderGame();
+  }
 }
 
 function renderGame(){
+  // Prev numbers Display
+  let nums_str = ""
+  for (let i = 0; i < nums.length; i++){
+    nums_str += " " + nums[i];
+  }
+  numbersEl.textContent = nums_str;
 
+  // Other displays
+  scoreEl.textContent = currentNumber;
+  payoutEl.textContent = payout;
+  creditsEl.textContent = credits;
 }
+
+// Returns a random  number between start and stop
+function getRandomNum(start, stop){
+  let randomNum = Math.floor(Math.random()*stop) + start;
+  return randomNum;
+}
+
+function pickFourToSeven(){
+  if (isAlive){
+    let num = getRandomNum(4, 7);
+    nums.push(num);
+    currentNumber += num;
+    renderGame();
+  }
+}
+
+  function pickOneToEight(){
+    if (isAlive){
+      let num = getRandomNum(1, 8);
+      nums.push(num);
+      currentNumber += num;
+      renderGame();
+    }
+  }
